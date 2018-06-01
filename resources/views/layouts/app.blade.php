fff40524531567ecb47036f5d6ed8e432b037d6d
Tom Eagle <tom@dxdigital.co.uk>
2018-06-01 20:09:39 +0100

    Bootstrap and jQuery scripts, Bootstrap navbar, jumbotron,

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,13 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/services">Services</a>
+        @include('inc.navbar')
+        
     <!-- this pulls in the content from the relevant page -->
         <div class="container">
             @yield('content')
         </div>
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
