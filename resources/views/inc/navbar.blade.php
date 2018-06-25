23d5461677bce078495c5538a8b42d3fef37b1cc
Tommy Eagle <tomeagle79@gmail.com>
2018-06-25 21:26:52 +0100

    Mods to navbar for logged in user etc

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -19,37 +19,21 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-                <ul class="nav navbar-nav">
                   <li class="active"><a href="/">Home<span class="sr-only">(current)</span></a></li>
                   <li><a href="/services">Services</a></li>
                   <li><a href="/about">About</a></li>
                   <li><a href="/posts">Blog</a></li>
-                  {{-- <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li role="separator" class="divider"></li>
-                      <li><a href="#">Separated link</a></li>
-                      <li role="separator" class="divider"></li>
-                      <li><a href="#">One more separated link</a></li>
-                    </ul>
-                  </li> --}}
                 </ul>
 
                 <!-- Right Side Of Navbar -->
                  <ul class="nav navbar-nav navbar-right">
-                  <li><a href="/posts/create">Create post</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
+                  
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
+                    <li><a href="/posts/create">Create post</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
