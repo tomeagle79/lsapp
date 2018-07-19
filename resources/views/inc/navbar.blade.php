dff3566504223c533a0b6382085b1b0a43d6d1fd
Tom Eagle <tom.eagle@reckless.agency>
2018-07-19 10:56:44 +0100

    npm and yarn install

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -33,15 +33,19 @@
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
-                    <li><a href="/posts/create">Create post</a></li>
+                        <li>
+                          <a href="/posts/create">Create post</a>
+                        </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                              {{ Auth::user()->name }} <span class="caret"></span>
+                          </a>
 
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
+                          <ul class="dropdown-menu" role="menu">
+                            <li>
+                              <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
+                            </li>
+                          </ul>
                         </li>
                     @endif
                 </ul>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,10 @@
     <small>Written on {{App\Post::parseDate($post->created_at)}}</small>
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit post</a>
-      {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST']) !!}
+      {!! Form::open([
+        'action' => ['PostsController@destroy', $post->id], 
+        'method' => 'POST', 
+        'class' => 'pull-right']) !!}
       <!-- The hidden field here is to 'spoof' a PUT request, as the update method can only be a PUT or patch -->
       {{Form::hidden('_method', 'DELETE')}}
       {{Form::submit('Delete post', ['class' => 'btn btn-danger'])}}
